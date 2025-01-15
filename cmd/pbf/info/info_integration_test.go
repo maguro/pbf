4a336158cd3ff3dd8958d6849653395ae383557e
Alan D. Cabrera <adc@toolazydogs.com>
2025-01-14 16:38:13 -0800

    Bump copyright date

--- cmd/pbf/info/info_integration_test.go
+++ cmd/pbf/info/info_integration_test.go
@@ -1,7 +1,7 @@
 //go:build integration
 // +build integration
 
-// Copyright 2017-24 the original author or authors.
+// Copyright 2017-25 the original author or authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
